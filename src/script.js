65123478561c47eab4db46405aaa5b1bf9e03df9
AbigailAwuni0 <abigailawuni0gmail.com>
2026-04-01 09:30:41 -0400

    add new features

---
+++ src/script.js
@@ -0,0 +1,16 @@
+alert(
+"Hello, User"    
+)
+
+let name = "Abigail"
+
+alert(name)
+let age = 20
+alert(age)
+
+let userName = prompt("What is your name?");
+alert("Hello," + userName);
+alert(`Hello ${userName} Good to see you. You are ${age} years`);
+
+let sentence = "I" + "am" +  "here";
+alert(sentence)
